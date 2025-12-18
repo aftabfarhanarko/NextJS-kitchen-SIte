d2b21ab3d45b339b78488a45846ec9788b2331d8
Aftab Farhan <aftabfarhan324@gmail.com>
2025-12-18 20:39:49 +0600

    get delet funcnilitey add now

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -1,7 +1,18 @@
-import { feedback } from "../../route";
+import { connect } from "@/app/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+const feedbackCollections = connect("feedback");
 
 export async function GET(request, {params}) {
     const {id} = await params;
-    const singleFeedBack = feedback.find(f => f.id == id);
-  return Response.json(singleFeedBack);
+    const query = {_id: new  ObjectId(id)};
+    const result = await feedbackCollections.findOne(query);
+  return Response.json(result);
+}
+// DELETE
+export async function DELETE(request, {params}) {
+    const {id} = await params;
+    const query = {_id: new  ObjectId(id)};
+    const result = await feedbackCollections.deleteOne(query);
+  return Response.json(result);
 }
