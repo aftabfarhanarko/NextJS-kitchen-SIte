1cde25260d175f6f177d6afb80f5afb2643220fe
Aftab Farhan <aftabfarhan324@gmail.com>
2025-12-18 20:51:36 +0600

    set updeat  funcnilitey add now

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -3,16 +3,29 @@ import { ObjectId } from "mongodb";
 
 const feedbackCollections = connect("feedback");
 
-export async function GET(request, {params}) {
-    const {id} = await params;
-    const query = {_id: new  ObjectId(id)};
-    const result = await feedbackCollections.findOne(query);
+export async function GET(request, { params }) {
+  const { id } = await params;
+  const query = { _id: new ObjectId(id) };
+  const result = await feedbackCollections.findOne(query);
   return Response.json(result);
 }
+
+// Patch
+export async function PATCH(request, { params }) {
+  const { id } = await params;
+  const { message } = await request.json();
+  const query = { _id: new ObjectId(id) };
+  const seter = {
+    $set: { message },
+  };
+  const result = await feedbackCollections.updateOne(query, seter);
+  return Response.json(result);
+}
+
 // DELETE
-export async function DELETE(request, {params}) {
-    const {id} = await params;
-    const query = {_id: new  ObjectId(id)};
-    const result = await feedbackCollections.deleteOne(query);
+export async function DELETE(request, { params }) {
+  const { id } = await params;
+  const query = { _id: new ObjectId(id) };
+  const result = await feedbackCollections.deleteOne(query);
   return Response.json(result);
 }
